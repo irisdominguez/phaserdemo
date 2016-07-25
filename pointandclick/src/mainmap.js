90704a1b6b0f6a8946f3b69046824bf2e23747fd
irisontherise <irisontherise@openmailbox.org>
2016-07-25 22:53:50 +0200

    Refactor code

--- pointandclick/src/mainmap.js
+++ pointandclick/src/mainmap.js
@@ -10,23 +10,23 @@ mainMap.prototype = {
     },
   	create: function(){
         // Level data
-        leveldata = JSON.parse(this.game.cache.getText('mainlevel'));
+        leveldata = JSON.parse(game.cache.getText('mainlevel'));
         console.log(leveldata);
 
-		var background = this.game.add.tileSprite(0,0,4320,900,"monkeybg");
+		var background = game.add.tileSprite(0,0,4320,900,"monkeybg");
 		background.anchor.setTo(0,0);
-        background.scale.setTo(this.game.height / background.height);
+        background.scale.setTo(game.height / background.height);
 
-        this.game.world.setBounds(0, 0, background.width * background.scale.x,
+        game.world.setBounds(0, 0, background.width * background.scale.x,
                 background.height * background.scale.y);
 
-		player = this.game.add.sprite(0, 0, "miguy");
+		player = game.add.sprite(0, 0, "miguy");
 		player.anchor.setTo(0.5, 1.0);
         player.scale.setTo(0.5);
-        this.game.camera.follow(player);
+        game.camera.follow(player);
 
         // Interaction
-        this.game.input.mouse.capture = true;
+        game.input.mouse.capture = true;
 	},
     update: function(){
         this.updateTarget();
@@ -42,10 +42,10 @@ mainMap.prototype = {
         
     },
     updateTarget: function(){
-        if (this.game.input.activePointer.leftButton.isDown)
+        if (game.input.activePointer.leftButton.isDown)
         {
             let playerPosition = new Phaser.Point(player.x, player.y);
-            let mousePosition = Phaser.Point.add(this.game.input.activePointer.positionDown, this.game.camera);
+            let mousePosition = Phaser.Point.add(game.input.activePointer.positionDown, game.camera);
             let wpi;
             let best = target;
             let minDist = null;
