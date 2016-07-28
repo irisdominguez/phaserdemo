a5a9e85cd585757c417fd6675c2e18f0815d4d12
irisontherise <irisontherise@openmailbox.org>
2016-07-28 11:08:38 +0200

    Finished astar algorithm

--- astar/astar.js
+++ astar/astar.js
@@ -4,6 +4,7 @@ function astarPreProcess(graph) {
     {
         let c = graph.connections[ci];
         let d = pointDistance(graph.nodes[c.p1], graph.nodes[c.p2]);
+        graph.connections[ci].d = d;
     }
 }
 
@@ -26,6 +27,9 @@ function findPath(g, i, j) {
     initialNode.p = null;
     while (openList.length > 0)
     {
+        console.log("OpenList: ");
+        console.log(openList);
+
         let currentNode = openList[0];
         let tmpIndex = 0;
         for (let i in openList)
@@ -41,6 +45,7 @@ function findPath(g, i, j) {
         if (currentNode.i == finalNode.i) 
         {
             let ret = [];
+            ret.push(currentNode.i);
             while (currentNode.p != null)
             {
                 ret.push(currentNode.p.i);
@@ -52,7 +57,53 @@ function findPath(g, i, j) {
         closedList.push(currentNode);
         for (let v in g.connections)
         {
-            if (g.connections[v].p1 != currentNode.i) continue;
+            if (g.connections[v].p1 != currentNode.i &&
+                g.connections[v].p2 != currentNode.i) continue;
+            let ni = g.connections[v].p1;
+            if (ni == currentNode.i) ni = g.connections[v].p2;
+
+            let closedNode = false;
+            for (let ci in closedList)
+            {
+                if (closedList[ci].i == ni) closedNode = true;
+            }
+            if (closedNode) continue;
+
+            let neighbor = {};
+            neighbor.i = ni;
+            
+            tmpg = currentNode.g + g.connections[v].d;
+            let bestScore = false;
+            let seen = false;
+            
+            for (let ci in openList)
+            {
+                if (openList[ci].i == neighbor.i)
+                {
+                    seen = true;
+                    if (tmpg < openList[ci].g)
+                    {
+                        neighbor = openList[ci];
+                        bestScore = true;
+                    }
+                }
+            }
+            if (!seen)
+            {
+                bestScore = true;
+                neighbor.x = g.nodes[neighbor.i].x;
+                neighbor.y = g.nodes[neighbor.i].y;
+                neighbor.h = pointDistance[neighbor, finalNode];
+                openList.push(neighbor);
+            }
+
+            if (bestScore)
+            {
+                neighbor.p = currentNode;
+                neighbor.g = tmpg;
+                neighbor.f = neighbor.g + neighbor.h;
+            }
         }
     }
+    return [];
 }
